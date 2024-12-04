ce961e865c59beda7c23e75131e268de8cefd562
Omer Sekmen <omerfaruk-sekmen@hotmail.com>
2024-12-04 17:18:53 +0300

    Degiskenler olusturuldu ve degerler atandi.

---
+++ odev1.kt
@@ -0,0 +1,100 @@
+package com.omerfaruksekmen.odev1omerfaruksekmen.odev1
+
+fun main(){
+
+    // ilçe
+    val ilce = "Kadıköy"
+    // kıta
+    val kita = "Avrupa"
+    // faks
+    var faks = "02121234567"
+    // posta kodu
+    val postaKodu = 34744
+    // insta adresi
+    var instaAdresi = "techcareernet"
+    // çalıştığın bölüm
+    val calisilanBolum = "Bilgisayar Mühendisliği"
+    // ürün miktarı
+    var urunMiktari = 150
+    // müşteri soyadı
+    val musteriSoyadi = "Dumbledore"
+    // ödeme miktarı
+    var odemeMiktari = 45.67
+    // doğum tarihi
+    val dogumTarihi = "2024-12-03"
+    // borç
+    var borc = false
+    // medeni hal
+    var medeniHal = true
+    // video yorumu
+    var videoYorumu = "Video içeriği harika"
+    // ödeme saati
+    var odemeSaati = "09:00:00"
+    // eft miktarı
+    var eftMiktari = 1560.65
+    // satılan miktar
+    var satilanMiktar = 350
+    // telefon modeli
+    val telefonModeli = "Xiaomi Redmi Note 13"
+    // dergi adı
+    val dergiAdi = "All About Space"
+    // basım tarihi
+    val basimTarihi = "2020-01-01"
+    // zam miktarı
+    val zamMiktari = 0.45
+    // daire sayısı
+    val daireSayisi = 50
+    // enlem
+    val enlem = "36-42 Kuzey"
+    // boylam
+    val boylam = "26-45 Doğu"
+    // yemek adı
+    var yemekAdi = "Mercimek Çorbası"
+    // ürün fiyatı
+    val urunFiyati = 249.99
+    // şirket
+    val sirket = "Techcareer"
+    // video adı
+    val videoAdi = "Kotlin Programlama"
+    // müzik süresi
+    val muzikSuresi = "3:45"
+    // mekan puanı
+    val mekanPuani = 4.5
+    // dosya adı
+    val dosyaAdi = "kotlinOdev"
+    // resim formatı
+    val resimFormati = "png"
+    // renk
+    var renk = "Mavi"
+    // renk kodu
+    var renkKodu = "#0000ff"
+    // bilgisayar modeli
+    val bilgisayarModeli = "MCLG-X"
+    // ekran boyutu
+    val ekranBoyutu = "16 inç"
+    // kullanım süresi
+    var kullanimSuresi = 15
+    // basınç
+    val basinc = 'P'
+    // etkinlik günü
+    var etkinlikGunu = "20 Ağustos Salı"
+    // ödeme günü
+    var odemeGunu = "10 Temmuz Pazar"
+    // yolculuk çıkış tarihi
+    val yolculukCikisTarihi = "2023-01-01 09:00:00"
+    // mahalle adı
+    val mahalleAdi = "Çicek Mh."
+    // otobüs hattı
+    val otobusHatti = 'Z'
+    // kullanılan dakika
+    var kullanilanDakika = 120
+    // kargo takip no
+    val kargoTakipNo = "123456789"
+    // kupon süresi
+    val kuponSuresi = 5
+    // kupon kodu
+    val kuponKodu = "ABC123"
+    // fatura tarihi
+    val faturaTarihi = "2024-12-04 09:00:00"
+
+}
